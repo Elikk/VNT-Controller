271dc8be14cfca76e12a84799b08aa1d9d486f2e
Elijus.Sekonas@gmail.com <Elijus.Sekonas@gmail.com>
2016-07-12 18:41:51 +0300

    pcb pakeitimai ir monitoring block naujas projektas

---
+++ Code/Monitoringblock/MonitorBlock/MonitorBlock/MonitorBlock.c
@@ -0,0 +1,16 @@
+/*
+ * MonitorBlock.c
+ *
+ * Created: 2016.07.12 18:37:58
+ *  Author: Joris
+ */ 
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
