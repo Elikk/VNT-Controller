ec790b8620d18162d550b6b75f0538f16282f2bf
Elijus.Sekonas@gmail.com <Elijus.Sekonas@gmail.com>
2017-01-08 11:57:55 +0200

    bus work, finished pcb 1 layer

---
+++ Vnt2/AEL_VNT_Controller/AEL_VNT_Controller/AEL_VNT_Controller.c
@@ -0,0 +1,17 @@
+/*
+ * AEL_VNT_Controller.c
+ *
+ * Created: 2017.01.05 11:58:12
+ *  Author: Joris
+ */ 
+
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
